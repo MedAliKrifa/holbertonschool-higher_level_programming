dc38155a8b5c242ceff435a0edb45d830a33eb7d
MedAliKrifa <daliolimona@gmail.com>
2022-09-09 20:33:26 +0100

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     result = 0
     length = len(sys.argv)
     if length <= 1:
-        print()
+        print(0)
     else:
         for x in range(1, length):
             result = result + int(sys.argv[x])
-    print(f"{result}")
+        print(f"{result}")

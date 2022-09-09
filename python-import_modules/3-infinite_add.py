61f415ac09ad8da0e038b25b692d0bffc529a501
MedAliKrifa <daliolimona@gmail.com>
2022-09-09 20:28:39 +0100

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    length = len(sys.argv)
+    if length <= 1:
+        print(0)
+    else:
+        for x in range(1, length):
+            result = result + int(sys.argv[x])
+    print(f"{result}")

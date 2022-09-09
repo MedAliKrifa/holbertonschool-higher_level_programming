875d4aa0c0744c1d41c4297a34b2020c0b448373
MedAliKrifa <daliolimona@gmail.com>
2022-09-09 16:58:34 +0100

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for fizzbuzz in range(1,101):
-    if fizzbuzz % 15 == 0:
-        print("FizzBuzz ", end="")                                        
-    elif fizzbuzz % 3 == 0:    
-        print("Fizz ", end="")                                        
-    elif fizzbuzz % 5 == 0:        
-        print("Buzz ",  end="")                                    
-    else:
-        print(f"{fizzbuzz} ", end="")
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 15 == 0:
+            print("FizzBuzz ", end="")
+        elif x % 3 == 0:
+            print("Fizz ", end="")
+        elif x % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print(f"{x} ", end="")

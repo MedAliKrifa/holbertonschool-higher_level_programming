83d729f6bb72a76f7cab4ddb5c2cda0e6b9d7cd3
MedAliKrifa <daliolimona@gmail.com>
2022-09-15 20:40:28 +0100

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)        
+    length = len(sentence)       
     if length > 0: 
-        char = sentence[0] 
-    else: 
+        char = sentence[0]
+    else:
         char = None
     return (length, char)

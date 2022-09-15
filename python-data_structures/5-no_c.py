87faebfbab02bd29f44019f518cbb04f71fd93a8
MedAliKrifa <daliolimona@gmail.com>
2022-09-15 19:54:29 +0100

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = ""
-    for x in new_str:
-        if x !="c" and x != "C":
-            new_str = str + x
-    return new_str
+    str = ""
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            str = str + x
+    return str

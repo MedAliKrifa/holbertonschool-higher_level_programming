25e25065f8296037ae6151b438766f5bf3f5036a
MedAliKrifa <daliolimona@gmail.com>
2022-09-20 19:19:45 +0100

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class square to define a square"""
+
+
+class Square:
+    """class square to define a square"""
+    pass
+    def __init__(self, size):
+        pass
+        self.__size = size

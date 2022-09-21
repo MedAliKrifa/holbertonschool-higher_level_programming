7a3e1b37fd383c60d39b704b525179d633ad7b25
MedAliKrifa <daliolimona@gmail.com>
2022-09-21 18:48:54 +0100

    6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""class square to define a square"""
+
+
+class Square:
+    """class square to define a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.size > 0:
+            if self.position[1] > 0:
+                print("\n" * self.position[1], end="")
+            for i in range(self.size):
+                if self.position[0] > 0:
+                    print(" " * self.position[0], end="")
+                print("#" * self.size)
+        else:
+            print(end="\n")

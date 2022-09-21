02141058b4ae2c523982a7c6fe9340e08c403430
MedAliKrifa <daliolimona@gmail.com>
2022-09-21 09:32:29 +0100

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,11 +7,14 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
+
     def area(self):
         return self.__size * self.__size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):

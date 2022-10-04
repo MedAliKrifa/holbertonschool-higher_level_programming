0c5cf47663092b8b67668a35f8523b07e6484665
MedAliKrifa <daliolimona@gmail.com>
2022-10-04 10:40:02 +0100

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """function to read file using with"""
     with open(filename, mode='r', encoding='utf-8') as f:
         read_file = f.read()
         print(read_file, end="")

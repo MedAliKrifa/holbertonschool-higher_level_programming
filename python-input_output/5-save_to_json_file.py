63ea6b8118b23ed3268741b9c22d47202d2da3a8
MedAliKrifa <daliolimona@gmail.com>
2022-10-05 09:23:48 +0100

    5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task 0"""
+
+
+from encodings import utf_8
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function to write file using with"""
+    with open(filename, mode = 'w', encoding='utf-8') as f:
+        f.write (json.dumps(my_obj))

94c3425aec00d50159cdd2b3d4fe89741fede1dc
MedAliKrifa <daliolimona@gmail.com>
2022-10-06 15:25:05 +0100

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """ Rectangle """
 from models.base import Base
@@ -6,21 +7,20 @@ from models.base import Base
 class Rectangle(Base):
     """ class rectangle that inherits from base """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ initialsation """
+        """ init """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-
     def __str__(self):
-        """ rectangle's info """
+        """informations """
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"\
             .format(self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
-        """ args assign """
+        """ args assign"""
         attrb = ["id", "width", "height", "x", "y"]
         if (args):
             for i in range(len(args)):
@@ -31,5 +31,45 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ width """
+        """ width property """
         return self.__width
+
+    @width.setter
+    def width(self, w):
+        """ width setter """
+        if type(w) is not int:
+            raise TypeError("width must be an integer")
+        elif w <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = w
+
+    @property
+    def height(self):
+        """ height property """
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        """ height setter """
+        if type(h) is not int:
+            raise TypeError("height must be an integer")
+        elif h <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = h
+
+    @property
+    def x(self):
+        """ x property """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ x setter """
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x

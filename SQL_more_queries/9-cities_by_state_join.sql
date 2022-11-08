73dfc77d7467032761ad46a079b487d0a3d84fbf
MedAliKrifa <daliolimona@gmail.com>
2022-11-08 14:11:24 +0100

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- code to that creates the MySQL table
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = state_id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER by cities.id ASC;

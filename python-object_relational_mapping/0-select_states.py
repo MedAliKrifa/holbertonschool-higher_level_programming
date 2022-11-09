cceba07054653c64fee1ed0bcacfeeaf482c1cc2
MedAliKrifa <daliolimona@gmail.com>
2022-11-09 23:53:16 +0100

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""a script that lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
-if __name__ == "_main_":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    curs = db.cursor()
-    curs.execute("SELECT id, name FROM states ORDER BY id;")
-    for t in curs.fetchall():
-        print(t)
+"""script to get data in a db """
 
-    curs.close()
+if __name__ == '__main__':
+    """ get data from db """
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
     db.close()

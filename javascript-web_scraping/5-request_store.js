4095190685a4d5dd62a580f9bcb637710ec51ac3
MedAliKrifa <daliolimona@gmail.com>
2022-12-13 21:13:36 +0100

    5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,11 +6,11 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-        fs.writeFile(filePath, body, 'utf8', function (err) {
-            if (err) {
-                console.error(err);
-            }
-        });
-    }
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf8', function (err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });

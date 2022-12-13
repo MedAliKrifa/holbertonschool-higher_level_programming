cfcd528804c402b55a569cf3649889510afecce6
MedAliKrifa <daliolimona@gmail.com>
2022-12-13 21:10:06 +0100

    5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,11 +2,9 @@
 const request = require('request');
 const fs = require('fs');
 
-// Get the URL and file path from the command line arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Call the URL and save the response in the given file
 request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     fs.writeFile(filePath, body, 'utf8', function (err) {

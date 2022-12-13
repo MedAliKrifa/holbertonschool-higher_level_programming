4e4ae9b453343000cb1963c7da94188087c925ca
MedAliKrifa <daliolimona@gmail.com>
2022-12-13 21:21:52 +0100

    6-completed_tasks.js

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const endpoint = 'https://jsonplaceholder.typicode.com/todos';
+
+request(endpoint, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+
+    const counts = data.reduce((counts, todo) => {
+      if (todo.completed) {
+        counts[todo.userId] = (counts[todo.userId] || 0) + 1;
+      }
+      return counts;
+    }, {});
+
+    for (const userId in counts) {
+      console.log(`'${userId}': ${counts[userId]},`);
+    }
+  }
+});

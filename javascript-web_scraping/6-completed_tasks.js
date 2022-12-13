d59e635265924a476324e049f4a24b8ae836ec85
MedAliKrifa <daliolimona@gmail.com>
2022-12-13 21:23:57 +0100

    6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -3,18 +3,18 @@ const request = require('request');
 const endpoint = 'https://jsonplaceholder.typicode.com/todos';
 
 request(endpoint, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body);
+    if (!error && response.statusCode === 200) {
+        const data = JSON.parse(body);
 
-    const counts = data.reduce((counts, todo) => {
-      if (todo.completed) {
-        counts[todo.userId] = (counts[todo.userId] || 0) + 1;
-      }
-      return counts;
-    }, {});
+        const counts = data.reduce((counts, todo) => {
+            if (todo.completed) {
+                counts[todo.userId] = (counts[todo.userId] || 0) + 1;
+            }
+            return counts;
+        }, {});
 
-    for (const userId in counts) {
-      console.log(`'${userId}': ${counts[userId]},`);
+        for (const userId in counts) {
+            console.log(`{'${userId}': ${counts[userId]},}`);
+        }
     }
-  }
 });
